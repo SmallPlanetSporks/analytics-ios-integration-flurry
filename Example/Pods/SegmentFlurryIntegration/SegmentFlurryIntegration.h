d8f2d495780470f78386efb6df66138f3ad24b7e
Geoff Pado <geoff@l4digital.com>
2016-12-06 14:33:36 -0800

    Add Carthage framework.

---
+++ Example/Pods/SegmentFlurryIntegration/SegmentFlurryIntegration.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegmentFlurryIntegration.
+FOUNDATION_EXPORT double SegmentFlurryIntegrationVersionNumber;
+
+//! Project version string for SegmentFlurryIntegration.
+FOUNDATION_EXPORT const unsigned char SegmentFlurryIntegrationVersionString[];
+
+#import <SegmentFlurryIntegration/SEGFlurryIntegration.h>
+#import <SegmentFlurryIntegration/SEGFlurryIntegrationFactory.h>
